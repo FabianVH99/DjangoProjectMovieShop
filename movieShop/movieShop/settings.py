b69669bbbc1b26977360c540079d500d538588ae
Fabian <vanhaelen.fabian@gmail.com>
2022-11-01 16:20:15 +0100

    Fixed error dotenv not found

--- movieShop/movieShop/settings.py
+++ movieShop/movieShop/settings.py
@@ -12,14 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
-import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
