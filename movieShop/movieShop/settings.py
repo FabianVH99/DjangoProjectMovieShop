bca46fd5ba6b12bd57602eeafe23664263b6f12f
Fabian <vanhaelen.fabian@gmail.com>
2022-11-03 16:25:09 +0100

    Updated settings

--- movieShop/movieShop/settings.py
+++ movieShop/movieShop/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i*oonbzak8bib9y%z00u!ah7=n*&5fp*(x83vn8(ibybpg%(!)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["django-project-webtopics.herokuapp.com", "127.0.0.1"]
 
