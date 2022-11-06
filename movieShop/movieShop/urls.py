ea2af86a13ab64895538437e9cbe4b52bd0c6d14
Fabian <vanhaelen.fabian@gmail.com>
2022-11-07 00:24:49 +0100

    Added filter + subscribe

--- movieShop/movieShop/urls.py
+++ movieShop/movieShop/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from shop.views import index, contact, product, subscribe
+from shop.views import index, contact, product, subscribe, sortGenre
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('<genre>/', sortGenre),
     path('contact/', contact),
     path('product/<id>/', product),
     path('subscribe/', subscribe),
--- movieShop/shop/views.py
+++ movieShop/shop/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from .models import Movie, Subscription
+from random import shuffle
 
 def index(request):
     movies = Movie.objects.all()
@@ -11,8 +12,10 @@ def contact(request):
 
 def product(request, id):
     movie = Movie.objects.get(id=id)
-    products = Movie.objects.filter(genre=movie.genre).exclude(id=id)
-    return render(request, 'product.html', {'movie':movie, 'products': products})
+    productList = list(Movie.objects.filter(genre=movie.genre).exclude(id=id))
+    shuffle(productList)
+    
+    return render(request, 'product.html', {'movie':movie, 'products': productList})
 
 def subscribe(request):
     post_name = request.POST.get('name', False)
@@ -21,3 +24,7 @@ def subscribe(request):
     new_item = Subscription(name = post_name, mail = post_email, movie_id = post_movie)
     new_item.save()
     return HttpResponseRedirect('/')
+
+def sortGenre(request, genre):
+    movies = Movie.objects.filter(genre=genre)
+    return render(request, 'index.html', {'movies':movies})
